5f2740361152c0e5a3217eb184a9c45a9a65f99b
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-03 14:29:21 +0100

    genera spring boot project

---
+++ server/src/test/java/me/ctrlmaniac/fairbnb/FairbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ctrlmaniac.fairbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
