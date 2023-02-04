11f64b960a77636f7c83bee30190ffb389f21fa1
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-04 21:57:47 +0100

    elimina con precisione la build di react

--- collectwww.sh
+++ collectwww.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-rm -rf server/src/main/resources/static
+rm -rf server/src/main/resources/static/assets
+rm server/src/main/resources/static/index.html
 cp -r www/dist server/src/main/resources/static
