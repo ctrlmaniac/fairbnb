bf08063caa7fc6973036f6ae7b670b0c28f88613
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-04 23:20:27 +0100

    collected client

--- collectwww.sh
+++ collectwww.sh
@@ -2,4 +2,5 @@
 
 rm -rf server/src/main/resources/static/assets
 rm server/src/main/resources/static/index.html
-cp -r www/dist server/src/main/resources/static
+cp -r www/dist/index.html server/src/main/resources/static/index.html
+cp -r www/dist/assets server/src/main/resources/static/assets
