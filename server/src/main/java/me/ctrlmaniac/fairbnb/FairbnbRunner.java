5333de26890fa6d9c84fb3ae41917b89edc1ec05
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-04 19:21:46 +0100

    sposta la cartella media in resources

--- server/src/main/java/me/ctrlmaniac/fairbnb/FairbnbRunner.java
+++ server/src/main/java/me/ctrlmaniac/fairbnb/FairbnbRunner.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
 
 import me.ctrlmaniac.fairbnb.entities.Account;
+import me.ctrlmaniac.fairbnb.entities.appartamento.Servizio;
 import me.ctrlmaniac.fairbnb.services.AccountService;
+import me.ctrlmaniac.fairbnb.services.appartamento.ServizioService;
+import me.ctrlmaniac.fairbnb.utils.DataLoader;
 
 @Component
 @Slf4j
@@ -16,12 +19,20 @@ public class FairbnbRunner implements CommandLineRunner {
 	@Autowired
 	AccountService accountService;
 
+	@Autowired
+	ServizioService servizioService;
+
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("Application started at http://localhost:8080");
 
 		Account account = new Account("davide.dicriscito@gmail.com", "Davide", "Di Criscito", "12345", "USER");
 		accountService.save(account);
+
+		// Carica i servizi
+		for (Servizio servizio : DataLoader.loadServiziFromCSV("media/csv/servizi.csv")) {
+			servizioService.save(servizio);
+		}
 	}
 
 }
---
+++ server/src/main/java/me/ctrlmaniac/fairbnb/utils/DataLoader.java
@@ -0,0 +1,46 @@
+package me.ctrlmaniac.fairbnb.utils;
+
+import java.util.List;
+
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import com.opencsv.CSVReader;
+
+import me.ctrlmaniac.fairbnb.entities.appartamento.Servizio;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DataLoader {
+
+	public static List<Servizio> loadServiziFromCSV(String filename) {
+		List<Servizio> servizi = new ArrayList<>();
+		File file = null;
+
+		try {
+			file = ResourceUtils.getFile("classpath:" + filename);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+
+		if (file != null) {
+			try {
+				CSVReader csvReader = new CSVReader(new FileReader(file.getPath()));
+
+				String[] values = null;
+
+				while ((values = csvReader.readNext()) != null) {
+					servizi.add(new Servizio(values[0]));
+				}
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			}
+		}
+
+		return servizi;
+	}
+}
