4bcd7efd1f01c7716a9b32b83be4c1fa4a90b31a
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-03-28 19:59:40 +0200

    init react-router-dom

--- www/src/main.tsx
+++ www/src/main.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { store } from "./store";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import theme from "./theme";
+import { Provider } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
 );
---
+++ www/src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+const Home: React.FC = () => {
+  return <Typography>Home</Typography>;
+};
+
+export default Home;
